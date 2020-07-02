3538d48aaa5d88bbba163f51618fc39616aff479
deiby1986 <deiby1986@gmail.com>
2020-07-02 08:20:53 -0500

    Creacion de mapper para el payload

---
+++ cocktail-restapi/src/main/java/com/cocktail/infraestructure/payload/PersonPayload.java
@@ -0,0 +1,18 @@
+package com.cocktail.infraestructure.payload;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class PersonPayload {
+	private Long id;
+	private String fullName;
+	private String lastName;
+	private String phone;
+	private String email;
+	private String birthDate;
+
+}
